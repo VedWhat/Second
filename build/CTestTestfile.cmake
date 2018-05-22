dd8d32f13b69b5406673d3b6f378dc9c5302b3f9
VedWhat <ved.chitnis@gmail.com>
2018-05-22 16:33:31 +0530

    Trial

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ved/ztrial/src
+# Build directory: /home/ved/ztrial/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
